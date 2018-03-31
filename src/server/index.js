260106d5f894d17e206a9d6928b850ce051c1a22
Luis Padron <heyluispadron@gmail.com>
2018-03-31 15:16:30 -0400

    Fix issue with babel

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,6 @@ import express from 'express'
 
 import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config'
 import { isProd } from '../shared/util'
-import renderApp from './render-app'
 
 const app = express()
 
@@ -12,7 +11,7 @@ app.use(STATIC_PATH, express.static('dist'))
 app.use(STATIC_PATH, express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send(renderApp(APP_NAME))
+  res.send(`${APP_NAME}`)
 })
 
 app.listen(WEB_PORT, () => {
