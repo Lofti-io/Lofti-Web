ce3a2f8540751e363ece4bc89a64b25a53d54bbd
Luis Padron <heyluispadron@gmail.com>
2018-04-02 19:10:06 -0400

    Update test suite to mocha/nyc

--- test/rootRoute.test.js
+++ test/rootRoute.test.js
@@ -1,9 +1,14 @@
 import request from 'supertest'
-
 import app from '../src/server/app'
 
-describe('GET /', () => {
-  it('should render properly', async () => {
-    await request(app).get('/').expect(200)
+describe("GET /",function(){
+  it("should return home page",function(done){
+    request(app)
+    .get("/")
+    .expect(200)
+    .end(function(err,res){
+      if (err) return done(err)
+      done()
+    })
   })
 })
